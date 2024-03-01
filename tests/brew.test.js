af5b6a59914f69860cf49daab5442ba4464410c0
b21phiro <philip.rosenqvist@outlook.com>
2024-03-01 11:44:45 +0100

    Added test for canvas creation upon init

--- tests/brew.test.js
+++ tests/brew.test.js
@@ -8,19 +8,34 @@ describe('brew.js', () => {
         }).toThrow(Error);
     });
 
-    test('Instantiate class with given root-element is the same', () => {
+    test('Instantiate class with no canvas ID works', () => {
         const rootElement = document.createElement('div');
         const brew = new Brew(rootElement);
+        expect(brew)
+            .toBeInstanceOf(Brew);
+    });
+
+    test('Instantiate class with given root-element is the same', () => {
+        const rootElement = document.createElement('div');
+        const brew = new Brew(rootElement, 'canvasID');
         expect(brew.rootElement)
             .toBe(rootElement);
     });
 
     test('Instantiate class with given canvas id is the same', () => {
         const rootElement = document.createElement('div');
-        const canvasID = 'canvas';
+        const canvasID = 'poop';
         const brew = new Brew(rootElement, canvasID);
         expect(brew.canvasElement.id)
             .toBe(canvasID);
+    });
+
+    test('Instantiate class mounts canvas onto root correctly', () => {
+        const rootElement = document.createElement('div');
+        const canvasID = 'poop';
+        const brew = new Brew(rootElement, canvasID);
+        expect(brew.rootElement.firstChild)
+            .toBe(brew.canvasElement);
     });
 
 });
