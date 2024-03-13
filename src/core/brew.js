bab8b4d196274cbaf9c80f3e73d54db0ba1e6673
b21phiro <philip.rosenqvist@outlook.com>
2024-03-13 16:35:34 +0100

    Initial creation of canvas element with webgl2 context

--- src/core/brew.js
+++ src/core/brew.js
@@ -3,63 +3,49 @@ export default class Brew {
     /** @type HTMLElement */
     rootElement = null;
 
-    /** Default ID for the canvas element. */
-    canvasElementIDDefault = "brewCanvas";
-
-    /** @type string */
-    canvasElementID = "";
-
-    /** @type HTMLElement<canvas> */
+    /** @type HTMLCanvasElement */
     canvasElement = null;
-
-    /** @type Number */
-    canvasWidth = 720;
-    /** @type Number */
-    canvasHeight = 540;
 
     /** @type WebGL2RenderingContext */
     canvasContext = null;
 
     /**
      * @param rootElement { HTMLElement }
-     * @param canvasElementID { string }
      */
-    constructor(rootElement, canvasElementID = "") {
+    constructor(rootElement) {
 
-        // Throw error if no root-element was given.
-        if (!rootElement) {
-            throw new Error("[Brew] Requires an element to mount the \"<canvas>\" element on but no element was given!");
-        }
-
-        // Set the root element to the given element.
+        // Sets the root element.
         this.rootElement = rootElement;
 
-        // Prepare the canvas element.
-        // If no id is given, set default canvas id.
-        this.canvasElement = document.createElement('canvas');
-        this.canvasElementID = (canvasElementID !== '')
-            ? canvasElementID
-            : this.canvasElementIDDefault;
-        this.canvasElement.id = canvasElementID;
+        // Setup canvas element.
+        this.initCanvasElement();
 
-        // Sets the context to be of webgl2.
-        this.canvasContext = this.canvasElement.getContext('webgl2');
-
-        // Sets the width and height of the canvas.
-        this.canvasElement.width = this.canvasWidth;
-        this.canvasElement.height = this.canvasHeight;
-
-        // Clear the root element upon mounting.
-        while(rootElement.children.firstChild) {
-            rootElement.children.removeChild(
-              rootElement.children.lastChild
-            );
+        // Ensure that the context is set.
+        if (!this.canvasContext) {
+            console.error("[Brew] Failed to set correct context for the canvas element!");
+            return false;
         }
 
-        // Mounts the canvas onto given root-element.
-        rootElement.insertAdjacentElement('afterbegin', this.canvasElement);
+        // Ready!
 
-        // ... Ready!
+    }
+
+    /**
+     * Creates a canvas element and mounts it onto the given root element.
+     */
+    initCanvasElement() {
+        const canvas = document.createElement('canvas');
+        const id = 'canvas';
+        canvas.id = id;
+        canvas.width = 740;
+        canvas.height = 540;
+
+        this.rootElement.insertAdjacentElement('afterbegin', canvas);
+
+        const element = document.getElementById(id);
+        this.canvasElement = element;
+
+        this.canvasContext = element.getContext('webgl2');
 
     }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-// Prevents certain actions from being taken and throws more exceptions.
-'use strict';
-
 import Brew from "./core/brew.js";
+
+/**
+ * The bootstrapper creates an instance of the Brew class.
+ * It knows how to properly initiate the library correctly.
+ */
+
+/** Prevents certain actions from being taken and throws more exceptions. */
+'use strict';
 
 /** @type Brew */
 let instance = null;
 
 /**
  * @param root { HTMLElement }
- * @param canvasID { string }
  */
-export default function brew(root, canvasID = "") {
-    if (!instance) {
-        instance = new Brew(root, canvasID);
+export default function brewInstance(root) {
+
+    // Throws error if ni root element was given.
+    if (!root) {
+        console.error("[BrewJS] Failed to bootstrap, as no root element was given to mount the canvas on!");
+        return false;
     }
+
+    // Creates an instance if non has been made allready.
+    if (!instance) {
+        instance = new Brew(root);
+    }
+
     return instance;
+
 }
